43c011e91171f8e73598d45d1f1faf68accdfb0f
Murat Mustafin <mustafinmurat@gmail.com>
2016-03-20 19:55:02 +0600

    Styled main screen

--- app/src/main/java/flat56/kazlogoquiz/activities/MainActivity.java
+++ app/src/main/java/flat56/kazlogoquiz/activities/MainActivity.java
@@ -2,11 +2,17 @@ package flat56.kazlogoquiz.activities;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import flat56.kazlogoquiz.R;
 
@@ -14,20 +20,33 @@ import flat56.kazlogoquiz.R;
 public class MainActivity extends Activity {
 
     private Button play;
+    private ImageButton dict;
+    private ImageButton share;
+    private ImageButton like;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        dict = (ImageButton) findViewById(R.id.dict);
+        share = (ImageButton) findViewById(R.id.share);
+        like = (ImageButton) findViewById(R.id.like);
+
+        int color_blue = getResources().getColor(R.color.main_blue);
+        int color_violet = getResources().getColor(R.color.main_violet);
+        ((GradientDrawable)share.getBackground().getCurrent()).setColor(color_blue);
+        ((GradientDrawable)like.getBackground().getCurrent()).setColor(color_violet);
+
+
+
+
 
         play = (Button)findViewById(R.id.play);
-        play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, LevelsActivity.class);
-                startActivity(intent);
-            }
+        play.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LevelsActivity.class);
+            startActivity(intent);
         });
     }
 
